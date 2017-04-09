f0b8be1fc99eca7be629184dc18e4c47be58261b
emre-edu-tech <emrekayikcilar@gmail.com>
2017-04-09 12:18:19 +0300

    modal javascript and transition effects added

--- app.js
+++ app.js
@@ -10,9 +10,25 @@ $(document).ready(function(){
       .closest('.news-item')
       .addClass('is-liked');
   });
-
+  //-----------------------------------------------
+  //     Add link
+  //------------------------------------------------
   $('.js-add-link').on('click', function(event){
     event.preventDefault();
     $('.js-form').toggleClass('is-visible');
+  });
+
+  //-----------------------------------------------
+  //     show modal
+  //------------------------------------------------
+  $('.js-show-modal').on('click', function(event){
+    event.preventDefault();
+    $('.js-modal').addClass('is-visible');
+    $('.js-modal-overlay').addClass('is-visible');
+  });
+
+  $('.js-modal-overlay').on('click', function(event){
+    $('.js-modal').removeClass('is-visible');
+    $('.js-modal-overlay').removeClass('is-visible');
   });
 });
